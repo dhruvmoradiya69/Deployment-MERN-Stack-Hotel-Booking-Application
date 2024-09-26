8438c4e3d119aec39292bc291efcb6783c536c8b
dhruvmoradiya0 <dhruvmoradiya69000@gmail.com>
2024-09-26 12:54:38 +0530

    add My Profile

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="bg-blue-800 py-10">
       <div className="container mx-auto flex justify-between items-center">
         <span className="text-3xl text-white font-bold tracking-tight">
-          HotelHub
+        <Link to="/">HotelHub</Link>
         </span>
-        <span className="text-white font-bold tracking-tight flex gap-4">
-          <p className="cursor-pointer">Privacy Policy</p>
-          <p className="cursor-pointer">Terms of Service</p>
+        <span className="text-white font-bold tracking-tight flex gap-2">
+          <p className="cursor-pointer flex items-center text-white px-3 font-bold hover:bg-blue-600 hover:rounded-sm">Privacy Policy</p>
+          <p className="cursor-pointer flex items-center text-white px-3 font-bold hover:bg-blue-600 hover:rounded-sm">Terms of Service</p>
         </span>
       </div>
     </div>
