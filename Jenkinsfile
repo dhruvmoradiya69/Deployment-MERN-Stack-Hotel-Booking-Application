17f5d01cae86a29f6e0b969bc7c57d69b099ea6c
dhruvmoradiya69 <dhruvmoradiya69000@gmail.com>
2024-11-17 09:49:10 +0530

    new add jenkins docker

---
+++ Jenkinsfile
@@ -0,0 +1,43 @@
+pipeline {
+
+    agent any
+ // agent { label 'worker' } 
+
+    stages{
+        stage('Clone Repository'){
+            steps{
+                git url: "https://github.com/dhruvmoradiya1/Hotelhub.git", branch: "dev"
+            }
+        }
+        stage('Build Frontend Image'){
+            steps{
+                sh "docker build -t mern-frontend:latest ./frontend"
+            }
+        }
+        stage('Build Backend Image'){
+            steps{
+                sh "docker build -t mern-backend:latest ./backend"
+            }
+        }
+
+        stage("Push Docker Images"){
+            steps{
+                 withCredentials([usernamePassword(
+                    credentialsId:"dockerhubid",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+                sh "docker image tag mern-frontend:latest ${env.dockerHubUser}/mern-frontend:latest"
+                sh "docker image tag mern-backend:latest ${env.dockerHubUser}/mern-backend:latest"
+                sh "docker push ${env.dockerHubUser}/mern-frontend:latest"
+                sh "docker push ${env.dockerHubUser}/mern-backend:latest"
+                }
+            }
+        }
+        stage('Deploy Application'){
+            steps{
+                sh "docker compose down && docker compose up -d --build"
+            }
+        }
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,32 +5,66 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
-import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import myHotelRoutes from "./routes/my-hotels";
 import hotelRoutes from "./routes/hotels";
 import myProfileRoutes from "./routes/my-profile";
 
+// Cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+// MongoDB connection
+const connectToDatabase = async () => {
+  const clusterURI = process.env.MONGODB_CONNECTION_STRING as string;
+  const localURI = process.env.MONGODB_CONNECTION_STRING_LOCAL as string;
 
+  try {
+    console.log("Trying to connect to MongoDB Cluster...");
+    await mongoose.connect(clusterURI);
+    console.log("MongoDB connected to Cluster Database");
+  } catch (err) {
+    console.error("Cluster connection failed. Trying to connect to Localhost MongoDB...");
+    try {
+      await mongoose.connect(localURI);
+      console.log("MongoDB connected to Localhost Database");
+    } catch (localErr) {
+      console.error("Failed to connect to both Cluster and Localhost MongoDB:", localErr);
+      process.exit(1); // Exit the process if connection fails
+    }
+  }
+};
+
+connectToDatabase();
+
+// Initialize express app
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (!process.env.FRONTEND_URL || !process.env.FRONTEND_URL_AWS) {
+  throw new Error('Both FRONTEND_URL and FRONTEND_URL_AWS environment variables must be defined.');
+}
+
+// Apply the CORS middleware with the allowed origins
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [ 
+      process.env.FRONTEND_URL, 
+      process.env.FRONTEND_URL_AWS
+    ],
     credentials: true,
   })
 );
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("🚀 Server is up and running, MongoDB is connected, and we're live on the web! 🌐");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -38,10 +72,7 @@ app.use("/api/my-hotels", myHotelRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/my-profile", myProfileRoutes);
 
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
-
-app.listen(3000, () => {
-  console.log("server running on localhost:3000");
+// Start the server
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on http://localhost:3000 🚀");
 });
